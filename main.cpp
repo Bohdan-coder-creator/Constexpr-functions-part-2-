d1a3a385fd7fd8a9321980797ca23cbac7b281c0
Bohdan Yurchenko <bogdanyurchenko83@gmail.com>
2025-08-13 22:50:59 +0200

    Добавлен проект Error-Detection-and-Handling-test

--- main.cpp
+++ main.cpp
@@ -1,49 +1,112 @@
 #include <iostream>
+#include <limits>   // for std::numeric_limits
+#include <cctype>   // for std::tolower
+#include "Random.h" // https://www.learncpp.com/cpp-tutorial/global-random-numbers-random-h/
 
-double getDouble()
+// Получение валидного числа от пользователя
+int getGuess(int count, int min, int max)
 {
-    std::cout << "Enter a decimal number: ";
-    double x{};
-    std::cin >> x;
-    return x;
-}
-
-char getOperator() // this is bullshit
-{
-    std::cout << "Enter one of the following: +, -, *, or /: ";
-    char op{};
-    std::cin >> op;
-    return op;
-}
-
-void printResult(double x, char operation, double y)
-{
-    std::cout << x << ' ' << operation << ' ' << y << " is ";
-
-    switch (operation)
+    while (true)
     {
-    case '+':
-        std::cout << x + y << '\n';
-        return;
-    case '-':
-        std::cout << x - y << '\n';
-        return;
-    case '*':
-        std::cout << x * y << '\n';
-        return;
-    case '/':
-        std::cout << x / y << '\n';
-        return;
+        std::cout << "Guess #" << count << ": ";
+
+        int guess{};
+        std::cin >> guess;
+
+        if (std::cin.fail()) // если ввод не число
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Please enter a valid number!\n";
+            continue; // просим заново
+        }
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // чистим буфер
+
+        if (guess < min)
+        {
+            std::cout << "Your number can't be less than " << min << "!\n";
+            continue;
+        }
+        else if (guess > max)
+        {
+            std::cout << "Your number can't be more than " << max << "!\n";
+            continue;
+        }
+
+        return guess; // валидный ввод
+    }
+}
+
+// returns true if the user won, false if they lost
+bool playHiLo(int guesses, int min, int max)
+{
+    std::cout << "Let's play a game. I'm thinking of a number between "
+              << min << " and " << max << ". You have "
+              << guesses << " tries to guess what it is.\n";
+
+    int number{ Random::get(min, max) }; // загадываем число
+
+    for (int count{1}; count <= guesses; ++count)
+    {
+        int guess{ getGuess(count, min, max) };
+
+        if (guess > number)
+            std::cout << "Your guess is too high.\n";
+        else if (guess < number)
+            std::cout << "Your guess is too low.\n";
+        else
+        {
+            std::cout << "Correct! You won! The number was " << number << "\n";
+            return true;
+        }
+    }
+
+    std::cout << "Sorry, you lose. The correct number was " << number << "\n";
+    return false;
+}
+
+bool playAgain()
+{
+    while (true)
+    {
+        char ch{};
+        std::cout << "Would you like to play again (y/n)? ";
+        std::cin >> ch;
+
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Please enter y or n!\n";
+            continue;
+        }
+
+        ch = static_cast<char>(std::tolower(ch));
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        switch (ch)
+        {
+            case 'y': return true;
+            case 'n': return false;
+            default:
+                std::cout << "Please enter y or n!\n";
+                break;
+        }
     }
 }
 
 int main()
 {
-    double x{ getDouble() };
-    char operation{ getOperator() };
-    double y{ getDouble() };
+    constexpr int guesses{7};
+    constexpr int min{1};
+    constexpr int max{100};
 
-    printResult(x, operation, y);
+    do
+    {
+        playHiLo(guesses, min, max);
+    } while (playAgain());
 
+    std::cout << "Thank you for playing.\n";
     return 0;
-} // the right one is in main02.cpp
+}
