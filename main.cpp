2959d7ade9a034d98ae827c9d07c9618b324e99e
Bohdan Yurchenko <bogdanyurchenko83@gmail.com>
2025-08-15 22:03:22 +0200

    Добавлен проект Narrowing-conversions-...-initializers

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 
-void printInt(int x)
+void print(unsigned int u) // note: unsigned
 {
-    std::cout << x << '\n';
+    std::cout << u << '\n';
 }
 
 int main()
 {
-    printInt(2);
-
-    short s{ 3 }; // there is no short literal suffix, so we'll use a variable for this one
-    printInt(s); // numeric promotion of short to int
-
-    printInt('a'); // numeric promotion of char to int
-    printInt(true); // numeric promotion of bool to int
+    std::cout << "Enter an integral value: ";
+    int n{};
+    std::cin >> n; // enter 5 or -5
+    print(n);      // conversion to unsigned may or may not preserve value
 
     return 0;
 }
+/*Narrowing conversion is a potentially unsafe conversion where the destination type cannot store all possible values of the source type.
+
+Examples include: converting from float to int, from a larger int to a smaller one, and between signed and unsigned types.
+
+You should avoid narrowing conversions, and if they are necessary, perform them explicitly using static_cast.
+
+Brace initialization ({}) disallows narrowing conversions and will produce a compile error.
+
+Exception: if the source value is a constexpr and can be exactly represented, the conversion is not considered narrowing.
+
+Conversions from float to int are always considered narrowing, even if the value is constexpr and in range.*/
--- main02.cpp
+++ main02.cpp
@@ -1,78 +1,15 @@
-#include <iostream>
-#include <limits> // for std::numeric_limits
-
-void ignoreLine()
+void someFcn(int i)
 {
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-}
-
-double getDouble()
-{
-    while (true) // Loop until user enters a valid input
-    {
-        std::cout << "Enter a decimal number: ";
-        double x{};
-        std::cin >> x;
-
-        bool success { std::cin }; // Remember whether we had a successful extraction
-        std::cin.clear();          // Put us back in 'normal' operation mode (in case we failed)
-        ignoreLine();              // Ignore any additional input on this line (regardless)
-
-        if (success)               // If we actually extracted a value
-            return x;              // Return it (otherwise, we go back to top of loop)
-    }
-}
-
-char getOperator()
-{
-    while (true) // Loop until user enters a valid input
-    {
-        std::cout << "Enter one of the following: +, -, *, or /: ";
-        char operation{};
-        std::cin >> operation;
-
-        // Check whether the user entered meaningful input
-        switch (operation)
-        {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            return operation; // return it to the caller
-        default: // otherwise tell the user what went wrong
-            std::cout << "Oops, that input is invalid.  Please try again.\n";
-        }
-    } // and try again
-}
-
-void printResult(double x, char operation, double y)
-{
-    std::cout << x << ' ' << operation << ' ' << y << " is ";
-
-    switch (operation)
-    {
-    case '+':
-        std::cout << x + y << '\n';
-        return;
-    case '-':
-        std::cout << x - y << '\n';
-        return;
-    case '*':
-        std::cout << x * y << '\n';
-        return;
-    case '/':
-        std::cout << x / y << '\n';
-        return;
-    }
 }
 
 int main()
 {
-    double x{ getDouble() };
-    char operation{ getOperator() };
-    double y{ getDouble() };
+    double d{ 5.0 };
 
-    printResult(x, operation, y);
+    someFcn(d); // bad: implicit narrowing conversion will generate compiler warning
+
+    // good: we're explicitly telling the compiler this narrowing conversion is intentional
+    someFcn(static_cast<int>(d)); // no warning generated
 
     return 0;
 }
