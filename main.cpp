e075d96aecb6055b479c394adc377586de11a227
Bohdan Yurchenko <bogdanyurchenko83@gmail.com>
2025-08-24 02:55:08 +0200

    Добавлен проект Function-templates

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-#include "random.h"
 #include <iostream>
 
-void print(int x, int y)
+template <typename T> // this is the template parameter declaration defining T as a type template parameter
+
+T add(T x, T y) // this is the function template definition for add<T>
 {
-    std::cout << "x: " << x << '\n';
-    std::cout << "y: " << y << '\n';
+    return x + y;
 }
 
 int main()
 {
-    print(5);
-
-    return 0;
+    std::cout << add(1, 2) << "\n";
+    std::cout << add(1.23, 5.2324) << "\n";
+    std::cout << add(std::string("Hello "), std::string("There")) << "\n";
 }
+/*
+Function templates are a mechanism in C++ (and some other languages) that allow you to write a single function to work with different data types.
+
+Instead of creating multiple overloaded functions (for example, for int, double, string), 
+you can write one template, and the compiler will automatically generate the required versions.
+*/
